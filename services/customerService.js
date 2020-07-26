20fc86b26c9422da01199474b55302299f6beaee
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-26 13:21:20 +0100

    fix customer/all

--- services/customerService.js
+++ services/customerService.js
@@ -4,14 +4,24 @@ const transactionService = require("./transactionService");
 
 module.exports = {
   getCustomers: async (params) => {
-    let customers = await Customer.find(params);
+    let customers = await Customer.find(params)
+      .populate({ path: "store_ref_id" })
+      .exec();
     customers = await Promise.all(
       customers.map(async (customer) => {
         customer = customer.toObject();
         let transactions = await transactionService.getTransactions({
           customer_ref_id: customer._id,
         });
-        return { ...customer, transactions };
+        const { store_name, _id } = customer.store_ref_id;
+        return {
+          ...customer,
+          store_name,
+          storeName: store_name,
+          store_id: _id,
+          store_ref_id: _id,
+          transactions,
+        };
       })
     );
     return customers;
