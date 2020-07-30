af972285d2c1b0bbf91fb2c869e505300807bad2
Philip <55332143+nerdyphil@users.noreply.github.com>
2020-07-30 22:53:28 +0100

    Activity log model

---
+++ activity.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const activitySchema = new Schema(
+  {
+    store_admin_ref: {
+      type: Schema.Types.ObjectId,
+      ref: "store_admin",
+      required: true,
+    },
+    store_assistant_ref: {
+      type: Schema.Types.ObjectId,
+      ref: "storeAssistant",
+      required: true,
+    },
+    time: { type: Date },
+    operation: {
+      type: String,
+      enum: ["update", "delete", "create"],
+      object: {
+        updated_field1: String,
+        updated_field2: String
+      }
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = model("activity", activitySchema);
