485ca9cb42ac769605db3b094197ecbbd7346779
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-31 15:41:22 +0100

    sending sms to customer upon payment

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -2,6 +2,10 @@ const axios = require("axios");
 const { errorHandler } = require("./login_controler");
 const PaymentModel = require("../models/payment");
 const { signToken } = require("./login_controler");
+const africastalking = require("africastalking")({
+  apiKey: process.env.AFRICASTALKING_API_KEY,
+  username: process.env.AFRICASTALKING_USERNAME,
+});
 
 exports.create = async (req, res) => {
   try {
@@ -29,6 +33,19 @@ exports.create = async (req, res) => {
     }
     payment.data = response.data;
     await payment.save();
+    const message = `You have successfully made a payment of ${payment.data.amount} to a store on Mycustomer`;
+    const to = "+" + payment.data.customer.phone_number;
+    const sms = africastalking.SMS;
+    try {
+      const response = await sms.send({
+        to,
+        message: message,
+        enque: true,
+      });
+    } catch (error) {
+      //  An error occurred while trying to send an sms
+      console.log(error);
+    }
 
     return res.status(200).json({
       success: true,
