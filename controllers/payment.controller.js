93bb2c6e206fc72430d51dd4cffac884c491f1dc
Emmanuel Menyaga <sparklinsparky@gmail.com>
2020-07-31 12:28:31 +0100

    minimal token

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,34 +1,16 @@
 const axios = require("axios");
 const { errorHandler } = require("./login_controler");
 const PaymentModel = require("../models/payment");
-const { transactionService } = require("../services");
 const { signToken } = require("./login_controler");
 
 exports.create = async (req, res) => {
   try {
-    const transaction = await transactionService.getOneTransaction({
-      _id: req.params.transaction_id,
-    });
-    if (!transaction)
-      return res.status(404).json({
-        success: false,
-        message: "no transaction with that ID",
-        error: {
-          statusCode: 404,
-        },
-      });
     const api_key = signToken(
       {
-        user_role: "store_admin",
-        _id: transaction.store_admin_ref._id,
-        phone_number: transaction.store_admin_ref.identifier,
+        user_role: "super_admin",
       },
       "87600h"
     );
-    const payment = new PaymentModel({
-      transaction: req.params.transaction_id,
-      status: "unverified",
-    });
 
     const response = await axios.get(
       `https://api.flutterwave.com/v3/transactions/${payment.transaction}/verify`,
